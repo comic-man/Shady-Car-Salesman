19414e0da560f0b6b4d83f499b28037f7c00330e
David Weidenbenner <44938475+comic-man@users.noreply.github.com>
2021-09-20 19:29:49 -0500

    Maybe

--- src/app/raccon-car/raccon-car.component.ts
+++ src/app/raccon-car/raccon-car.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-raccon-car',
-  templateUrl: './raccon-car.component.html',
+  templateUrl:`<raccon-car></raccon-car>
+              <raccon-car></raccon-car>`,
   styleUrls: ['./raccon-car.component.css']
 })
 export class RacconCarComponent implements OnInit {
