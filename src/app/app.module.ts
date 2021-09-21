0ebfaad0382f815b8b6e6b40f434363b3f6839d8
David Weidenbenner <44938475+comic-man@users.noreply.github.com>
2021-09-20 19:53:39 -0500

    I don't know what I'm doing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NoTiresComponent } from './no-tires/no-tires.component';
 import { RacconCarComponent } from './raccon-car/raccon-car.component';
-import { MethheadCarComponent } from './methhead-car/methhead-car.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NoTiresComponent,
     RacconCarComponent,
-    MethheadCarComponent
   ],
   imports: [
     BrowserModule
--- src/app/raccon-car/raccon-car.component.ts
+++ src/app/raccon-car/raccon-car.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-raccon-car',
-  templateUrl:`<raccon-car></raccon-car>
-              <raccon-car></raccon-car>`,
+  template:`<img src="assets/raccooncar.jpg" alt="">`,
   styleUrls: ['./raccon-car.component.css']
 })
 export class RacconCarComponent implements OnInit {
